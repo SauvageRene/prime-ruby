98e1b610c60648040506c24fd9c66ddb4ba81c40
SauvageRene <renegarcia1864@gmail.com>
2020-10-26 16:11:57 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(natural)
+
+    if (natural < 0 or natural == 0 or natural == 1)
+        return false
+        else
+            (2..natural-1).to_a.all? do |possible_factor|
+                natural % possible_factor != 0
+        end
+    end
+end
+
